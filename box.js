7e64f858d3ee9b0e0eb2985fdac0a0b82cab6cc2
samriddhi7307 <72978417+samriddhi7307@users.noreply.github.com>
2020-11-25 13:37:18 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,30 @@
+class box
+{
+	constructor(x, y,width,height)
+	{
+		var options={
+			restitution: 1,
+			friction:0.3,
+			density:0.01
+			
+			}
+		
+		this.body=Bodies.rectangle(x, y,width,height, options);
+		this.width = width;
+		this.height = height;
+		this.y = y;
+		this.x = x;
+		World.add(world, this.body);
+
+	}
+	display()
+	{
+		var pos =this.body.position;
+      rectMode(CENTER);
+     strokeWeight(3);
+      rect(pos.x, pos.y, this.width, this.height);
+	}
+
+} 
+
+ 
